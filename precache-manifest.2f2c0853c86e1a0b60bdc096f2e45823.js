3b54815ee1e4f904a6e1894d7cba644a064034e1
Valdimar Björn Ásgeirsson <vrioniavas@gmail.com>
2019-04-02 17:44:47 +0000

    Initial commit

---
+++ precache-manifest.2f2c0853c86e1a0b60bdc096f2e45823.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "73a5aeb837826670f9f1ad30528e9a6a",
+    "url": "/static/media/Manga_014.73a5aeb8.jpg"
+  },
+  {
+    "revision": "1aa02efd5954d5ea34ef",
+    "url": "/static/css/main.4cd548e5.chunk.css"
+  },
+  {
+    "revision": "5b59d1acaab6e3f1a713",
+    "url": "/static/js/1.5b59d1ac.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "064e5be197c9cb6e866c82dfda518ac1",
+    "url": "/static/media/albatross.064e5be1.jpg"
+  },
+  {
+    "revision": "ec387ee49bd9f925c36d6992a77323a5",
+    "url": "/static/media/Ko-fi_Red.ec387ee4.png"
+  },
+  {
+    "revision": "9751cb018e208cc0391488499b926963",
+    "url": "/static/media/AlbatrossLogo_wide_text2_150.9751cb01.png"
+  },
+  {
+    "revision": "7b9513fd4b4cdb5c57429b4a5607610a",
+    "url": "/static/media/AlbatrossLogo_mid_text_150.7b9513fd.png"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/glyph-logo_May2016.0e84d592.png"
+  },
+  {
+    "revision": "5c16a0a139544142d624912b3b8b7843",
+    "url": "/static/media/bg1.5c16a0a1.jpg"
+  },
+  {
+    "revision": "54adce8d16d82e9e38b4a2a310a17df7",
+    "url": "/static/media/Manga_000.54adce8d.jpg"
+  },
+  {
+    "revision": "a5c6f90ff5f2f5b302d3b0af32f32827",
+    "url": "/static/media/Manga_002.a5c6f90f.jpg"
+  },
+  {
+    "revision": "a5aa030f9ab575fabaa6f0c3ff35f5c5",
+    "url": "/static/media/Manga_004.a5aa030f.jpg"
+  },
+  {
+    "revision": "abb4c262c0240a62b2319cec93b9988b",
+    "url": "/static/media/Manga_005.abb4c262.jpg"
+  },
+  {
+    "revision": "705b2ab6b4666776517f14f0dec30870",
+    "url": "/static/media/Manga_006.705b2ab6.jpg"
+  },
+  {
+    "revision": "2b3a3634da4ba2b48c5c7e42226d4e4c",
+    "url": "/static/media/Manga_007.2b3a3634.jpg"
+  },
+  {
+    "revision": "eb80f26642b6c815ca9338f39371c569",
+    "url": "/static/media/Manga_008.eb80f266.jpg"
+  },
+  {
+    "revision": "33f0bea0b34e947569164f052bcaea90",
+    "url": "/static/media/Manga_009.33f0bea0.jpg"
+  },
+  {
+    "revision": "75e4fba6bcd753571cc62fcad411f710",
+    "url": "/static/media/Manga_010.75e4fba6.jpg"
+  },
+  {
+    "revision": "843fa87d4606685582185ef521a10b92",
+    "url": "/static/media/Manga_011.843fa87d.jpg"
+  },
+  {
+    "revision": "ce220712f875b7c3b5887bddbd3df793",
+    "url": "/static/media/Manga_012.ce220712.jpg"
+  },
+  {
+    "revision": "00b808b8c207287729dc098af0661df2",
+    "url": "/static/media/Manga_013.00b808b8.jpg"
+  },
+  {
+    "revision": "1aa02efd5954d5ea34ef",
+    "url": "/static/js/main.1aa02efd.chunk.js"
+  },
+  {
+    "revision": "b7175461ddb41068189a2bb611d4dd66",
+    "url": "/static/media/Manga_015.b7175461.jpg"
+  },
+  {
+    "revision": "add14645c5b14891ba967fae9e4307fb",
+    "url": "/static/media/Manga_016.add14645.jpg"
+  },
+  {
+    "revision": "b82d55affc665d4c36aa79d0fbaa7df8",
+    "url": "/static/media/Manga_017.b82d55af.jpg"
+  },
+  {
+    "revision": "2b4537c9af7c12b3ea72dcbe4a021af1",
+    "url": "/static/media/Manga_018.2b4537c9.jpg"
+  },
+  {
+    "revision": "3dd8144558914db3b45ea472906acf29",
+    "url": "/static/media/Manga_019.3dd81445.jpg"
+  },
+  {
+    "revision": "b7c88880956bb02e8885683ec4f90682",
+    "url": "/static/media/Manga_020.b7c88880.jpg"
+  },
+  {
+    "revision": "d6c4fce1c474bff33bb2786443777834",
+    "url": "/static/media/Manga_026.d6c4fce1.jpg"
+  },
+  {
+    "revision": "09b6d7a079a98e1bad51c14982a1e33c",
+    "url": "/static/media/Jolamanga_001_Stekkjastaur.09b6d7a0.jpg"
+  },
+  {
+    "revision": "11338af675d2252d7212a04c4a412238",
+    "url": "/static/media/Jolamanga_002_Giljagaur.11338af6.jpg"
+  },
+  {
+    "revision": "0521eccaabe4481453c82bbb8cbd4b12",
+    "url": "/static/media/Jolamanga_003_Stufur.0521ecca.jpg"
+  },
+  {
+    "revision": "6e7d3a6fe7ed27e5ec7e877cd723051e",
+    "url": "/static/media/Jolamanga_004_Thvorusleikir.6e7d3a6f.jpg"
+  },
+  {
+    "revision": "09ab15cfaa903f6ad9b1b49ec5663403",
+    "url": "/static/media/Jolamanga_005_Pottaskefill.09ab15cf.jpg"
+  },
+  {
+    "revision": "2b21a66356ef7e6570572501a9886a53",
+    "url": "/static/media/Jolamanga_006_Askasleikir.2b21a663.jpg"
+  },
+  {
+    "revision": "761d150991081e28b8c8a6e1447af564",
+    "url": "/static/media/Jolamanga_007_Hurdaskellir.761d1509.jpg"
+  },
+  {
+    "revision": "011ab6f284a333c97b3c8c04b4ebf7bc",
+    "url": "/static/media/Jolamanga_008_Skyrgamur.011ab6f2.jpg"
+  },
+  {
+    "revision": "16bec20cb462f2fb9207a0664debfbc8",
+    "url": "/static/media/Jolamanga_009_Bjugnakraekir.16bec20c.jpg"
+  },
+  {
+    "revision": "0b798423ca8ce88105c67c47fe28b012",
+    "url": "/static/media/Jolamanga_010_Gluggagaegir.0b798423.jpg"
+  },
+  {
+    "revision": "78cdd56e2238ee2311ab898ff42a24ac",
+    "url": "/static/media/Jolamanga_011_Gattathefur.78cdd56e.jpg"
+  },
+  {
+    "revision": "f141f14363d1af589fc28034c60e7eed",
+    "url": "/static/media/Jolamanga_12_Ketkrokur.f141f143.jpg"
+  },
+  {
+    "revision": "d7b8f14ddbc6a15e0eba3562dbfdb355",
+    "url": "/static/media/Jolamanga_13_Kertasnikir.d7b8f14d.jpg"
+  },
+  {
+    "revision": "b92b99655ee0b4b1761c9da89a0a9db0",
+    "url": "/index.html"
+  }
+];
